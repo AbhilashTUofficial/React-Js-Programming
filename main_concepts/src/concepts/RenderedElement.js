38576c2b7e0b1040fa83493f1ad6a78946cc319e
abhilashTUofficial <abhilashstorm@gmail.com>
2022-09-29 10:46:07 +0530

    update

---
+++ main_concepts/src/concepts/RenderedElement.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+
+import "./styles.css";
+
+class RenderElements extends Component {
+  state = {};
+  render() {
+    return <body>
+
+        <div className="cont">hi</div>
+
+    </body>;
+  }
+}
+
+export default RenderElements;
